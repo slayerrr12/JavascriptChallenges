40b6c057f0abc23082c97124b05a669977e9ed5b
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-05 12:09:35 +0530

    challenge1 complete!

--- Challenge1/index.js
+++ Challenge1/index.js
@@ -1,2 +1,8 @@
-let js = "amazing";
-if (js === "amazing") console.log("amazing")
+
+
+
+const markBmi = 78 / (1.69 * 1.69)
+const johnBmi = 92 / (1.95 * 1.95)
+
+const markHigherBmi = markBmi > johnBmi ? true : false;
+console.log(markHigherBmi)
