0dc0d0296568b94af5192603abc5a8a14a9fc275
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-13 21:13:07 +0530

    basic for loop example

---
+++ Loops/forLoop.js
@@ -0,0 +1,7 @@
+// for loops keeps running while the condition is true 
+// to alter the condition you can do any computational operation
+
+for (let rep = 1 ; rep <=10 ; rep++){
+    console.log(rep);
+    rep++;
+} 
